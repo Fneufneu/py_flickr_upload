a0bb1d747396bbd2de38fac14ecc7bfadc4edbde
Taylor Malloy <tmalloy@gmail.com>
2013-03-30 16:26:38 -0700

    Initial commit of code

---
+++ flickr_upload.py
@@ -0,0 +1,155 @@
+import sys
+import os
+import argparse
+import ConfigParser
+import subprocess
+import flickr_api
+
+
+class PhotoUploader(object):
+
+    def __init__(self, recreate_exif=False, upload_to_set=False, user=None):
+        self.upload_to_set = upload_to_set
+        self.recreate_exif = recreate_exif
+        self.user = user
+
+        if self.upload_to_set:
+            self.photosets = {}
+            photosets = user.getPhotosets() 
+            for photoset in photosets:
+                self.photosets[photoset.title] = photoset
+
+
+    def is_valid(self, file_path):
+        return os.path.splitext(file_path)[1].lower() in [".jpg", ".png"]
+
+    def upload_photo(self, file_path):
+        dir_path, name = os.path.split(file_path)
+
+        if not self.is_valid(file_path):
+            print "Ignoring '%s'" % file_path
+            return
+
+        if self.recreate_exif:
+            subprocess.check_call(["jhead", "-de", file_path],
+                                  stdout=subprocess.PIPE)
+            subprocess.check_call(["jhead", "-mkexif", file_path],
+                                  stdout=subprocess.PIPE)
+
+        flickr_photo = flickr_api.upload(photo_file=file_path,
+                                         title=name,
+                                         is_public=0)
+
+        if self.upload_to_set:
+            photoset_title = os.path.basename(dir_path.rstrip("/"))
+            self.add_to_photoset(flickr_photo, photoset_title)
+
+
+    def add_to_photoset(self, photo, photoset_title):
+        assert self.upload_to_set
+
+        # flickr_api seems to use unicode
+        photoset_title = unicode(photoset_title)
+
+        photoset = self.photosets.get(photoset_title, None)
+        if not photoset:
+            photoset = flickr_api.Photoset.create(title=photoset_title,
+                                                  primary_photo=photo)
+            self.photosets[photoset_title] = photoset
+        else:
+            photoset.addPhoto(photo=photo)
+
+
+
+
+def main():
+
+    args = parse_args()
+
+    print "Logging in..."
+
+    auth_file = "flickr.auth"
+    key_file = "keys.conf"
+
+    config = ConfigParser.RawConfigParser()
+    config.read(key_file)
+
+    flickr_api.set_keys(config.get("auth", "api_key"),
+                        config.get("auth", "api_secret"))
+
+    if not os.path.isfile(auth_file):
+        a = flickr_api.auth.AuthHandler()
+        url = a.get_authorization_url("write")
+        print ("Allow access to flickr through this url, then copy and "
+               "paste the oauth_verifier here")
+        print url
+
+        sys.stdout.write("\noauth_verifier: ")
+        oauth_verifier = raw_input()
+        a.set_verifier(oauth_verifier)
+        flickr_api.set_auth_handler(a)
+
+        a.save(auth_file)
+
+    else:
+        try:
+            flickr_api.set_auth_handler(auth_file)
+        except Exception as e:
+            os.remove(auth_file)
+            print "Need to reauthorize, run the script again"
+
+
+    user = flickr_api.test.login()
+
+    uploader = PhotoUploader(recreate_exif=args.recreate_exif,
+                             upload_to_set=args.put_in_sets,
+                             user=user)
+    print "Counting..."
+    paths_to_upload = []
+
+    for root, dirnames, fns in os.walk(args.input_folder):
+        for fn in fns:
+            if not uploader.is_valid(fn):
+                continue
+
+            file_path = os.path.join(root, fn)
+            paths_to_upload.append(file_path)
+
+
+    for i, file_path in enumerate(paths_to_upload):
+        name = os.path.basename(file_path)
+
+        restart_line()
+        sys.stdout.write("%d/%d: '%s'" % (i+1, len(paths_to_upload), name))
+
+        uploader.upload_photo(file_path)
+
+
+def restart_line():
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
+def directory(dir_path):
+    if os.path.isdir(dir_path):
+        return os.path.abspath(dir_path)
+    else:
+        raise argparse.ArgumentTypeError("Not a directory")
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Upload some flickr photos")
+
+    parser.add_argument("input_folder", type=directory,
+                        help="Folder containing photos and other folders")
+    parser.add_argument("-s", "--put-in-sets", action="store_true",
+                        help=("Uses the folder name containing the photos to "
+                              "place them in sets"))
+    parser.add_argument("-e", "--recreate-exif", action="store_true",
+                        help=("Replaces the exif data of the date taken with "
+                              "the creation date of the file"))
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    main()
---
+++ split_into_200.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import shutil
+
+input_folder_path = sys.argv[1]
+output_folder_path = sys.argv[2]
+
+class OutputFolder(object):
+
+    def __init__(self, path):
+        self.fns = []
+        self.path = path
+        if not os.path.isdir(path):
+            os.mkdir(path)
+
+
+    def copy_to_folder(self, file_path):
+        if self.is_full:
+            raise Exception("Im full!")
+
+
+        fn = os.path.basename(file_path)
+        if os.path.splitext(fn)[1].lower() not in [".jpg", ".png"]:
+            print "Ignoring %s" % file_path
+            return
+
+        output_fn = fn
+        while output_fn in self.fns:
+            output_fn = "_" + output_fn
+
+        self.fns.append(output_fn)
+        shutil.copyfile(file_path, os.path.join(self.path, output_fn))
+
+
+    @property
+    def is_full(self):
+        return len(self.fns) >= 200
+
+
+                        
+num_output_folders = 0
+current_output_folder = OutputFolder(os.path.join(output_folder_path,
+                                                  str(num_output_folders)))
+
+for root, dirs, files in os.walk(input_folder_path):
+    if not files:
+        continue
+
+    for fn in files:
+        if current_output_folder.is_full:
+            num_output_folders += 1
+            current_output_folder = OutputFolder(
+                os.path.join(output_folder_path, str(num_output_folders))
+            )
+
+
+        current_output_folder.copy_to_folder(os.path.join(root, fn))
+
+
